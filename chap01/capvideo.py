88b98cf38f46151244f3574b9fd8fe8733178f87
xkzhai-MacbookAir <xkzhai@MacbookAir.com>
2022-11-19 07:41:35 +0800

    read cap from video file

--- chap01/capvideo.py
+++ chap01/capvideo.py
@@ -5,7 +5,10 @@ cv2.namedWindow('video',cv2.WINDOW_NORMAL)
 cv2.resizeWindow('video',640,480)
 
 # 获取视频设备
-cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture(0)
+
+# 获取视频文件，从视频文件中读取视频帧
+cap = cv2.VideoCapture("/Users/xkzhai/OpenCV_Python/video/Beer.mp4")
 
 while True:
     # 从摄像头读取视频帧
@@ -15,7 +18,11 @@ while True:
     cv2.imshow('video',frame)
 
     # 等待键盘事件,如果为q，退出
-    key = cv2.waitKey(1)
+    # key = cv2.waitKey(1)
+    
+    # 需与视频文件的帧数一致
+    key = cv2.waitKey(40)
+    
     if(key & 0xFF == ord('q')):
         break
 
