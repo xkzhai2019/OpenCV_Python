a3421cf1aa87e02c2a57ff09545fddfc64cc8e22
xkzhai-MacbookAir <xkzhai@MacbookAir.com>
2022-11-20 20:04:47 +0800

    some modification

--- chap01/videoIO.py
+++ chap01/videoIO.py
@@ -7,29 +7,32 @@ fourcc = cv2.VideoWriter_fourcc('D','I','V','X')
 vw = cv2.VideoWriter("/Users/xkzhai/OpenCV_Python/video/out.avi",fourcc,25,(1280,720)) 
 # 创建窗口
 cv2.namedWindow('video',cv2.WINDOW_NORMAL)
-cv2.resizeWindow('video',1280,720)
+cv2.resizeWindow('video',640 ,360)
 
 # 获取视频设备
 cap = cv2.VideoCapture(0)
  
  
-while True:
+while cap.isOpened(): # 判断摄像头是否打开
     # 从摄像头读取视频帧
     ret, frame = cap.read()
 
-    # 将视频帧在窗口显示
-    cv2.imshow('video',frame)
+    if ret == True: 
+        # 将视频帧在窗口显示
+        cv2.imshow('video',frame)
+        cv2.resizeWindow('video',640 ,360) # 重新将窗口设置为指定大小
 
-    # 写数据到文件
-    vw.write(frame)
+        # 写数据到文件
+        vw.write(frame)
 
-    # 等待键盘事件,如果为q，退出
-    key = cv2.waitKey(1)
-    
-    
-    if(key & 0xFF == ord('q')):
+        # 等待键盘事件,如果为q，退出
+        key = cv2.waitKey(1)
+        
+        
+        if(key & 0xFF == ord('q')):
+            break
+    else:
         break
-
 # 释放VideoCapture
 cap.release()
 vw.release()
