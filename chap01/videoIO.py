16718f23a4b7d6422c6559961d5cd8769e158623
xkzhai-MacbookAir <xkzhai@MacbookAir.com>
2022-11-20 09:23:34 +0800

    write a video

---
+++ chap01/videoIO.py
@@ -0,0 +1,36 @@
+import cv2
+
+
+fourcc = cv2.VideoWriter_fourcc('D','I','V','X')
+
+# 创建输出文件
+vw = cv2.VideoWriter("/Users/xkzhai/OpenCV_Python/video/out.avi",fourcc,25,(1280,720)) 
+# 创建窗口
+cv2.namedWindow('video',cv2.WINDOW_NORMAL)
+cv2.resizeWindow('video',1280,720)
+
+# 获取视频设备
+cap = cv2.VideoCapture(0)
+ 
+ 
+while True:
+    # 从摄像头读取视频帧
+    ret, frame = cap.read()
+
+    # 将视频帧在窗口显示
+    cv2.imshow('video',frame)
+
+    # 写数据到文件
+    vw.write(frame)
+
+    # 等待键盘事件,如果为q，退出
+    key = cv2.waitKey(1)
+    
+    
+    if(key & 0xFF == ord('q')):
+        break
+
+# 释放VideoCapture
+cap.release()
+vw.release()
+cv2.destroyAllWindows()
